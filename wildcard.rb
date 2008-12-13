11cf67a1e439a2155575e751c4da51a82f8b04c8
Doug Peterson <dougp757@gmail.com>
2008-12-13 10:41:34 -0500

    added the ability to Class.this_X_or_that_X previously it only worked on instance variables.  This was very hard

--- wildcard.rb
+++ wildcard.rb
@@ -5,24 +5,35 @@ module WildCard
   def hello_X_how_are_you(name)
     "goodbye #{name}"
   end
-
-
-
+  #when the module is included overrides the class method of method missing so you can do MyClass.method type calls
+  def WildCard.included(mod)
+    def mod.method_missing(method_id, *arguments)
+    results=WildCard.wildcard(methods,method_id,*arguments)
+    if results then return send(results[0],*results[1]) end
+    super
+    end
+  end
 
   def method_missing(method_id , *arguments)
+    results=WildCard.wildcard(methods,method_id,*arguments)
+    if results then return send(results[0],*results[1]) end
+    super
+  end
+  def self.wildcard methods, method_id, *arguments
     letter="[A-Za-z0-9_]+"
     wildcards=methods.select{|x| x.index('X')}.map{|x| {:name=>x,:parts=>x.split('X')}}
     my_method=wildcards.detect{|x| 
       method_id.to_s.match(Regexp.new("^#{x[:parts].join(letter)}#{if x[:name][-1..-1]=='X' then letter end}$"))
     }
     if my_method then 
-      output=remove_strings(method_id.to_s,my_method[:parts]) + arguments
-      return self.send(my_method[:name], *output)
+      output=WildCard.remove_strings(method_id.to_s,my_method[:parts]) + arguments
+      return [my_method[:name], output]
+    else
+      return nil
     end
-    super
   end
-  private
-  def remove_strings(string, to_remove)
+  #recursive function that removes instances in to_remove from string and returns an array of the leftovers
+  def self.remove_strings(string, to_remove)
     if to_remove.size==0 then
       return (if string=="" then [] else [string] end)
     else
--- wildcard_test.rb
+++ wildcard_test.rb
@@ -1,5 +1,5 @@
 class WildCardTest
-  require "WildCard"
+  require "wildcard"
   include WildCard
   def the_X_stole_my_X(animal,something)
     "The #{animal} stole my fucking #{something}"
@@ -13,10 +13,23 @@ class WildCardTest
   def math_X_plus_X_equals(a,b,sum)
     a.to_i + b.to_i == sum
   end
+  def self.helloX(name)
+    name
+  end
 end
 
 a=WildCardTest.new
 if a.the_dog_stole_my_guitar == "The dog stole my fucking guitar" then puts "success" else raise "EPIC FAIL" end
-puts a.i_want_a_train_and_a_puppy_and_a_macbook_pro_the_new_one_and_a_cat_for_xmas
 if a.i_want_a_train_and_a_puppy_and_a_macbook_pro_the_new_one_and_a_cat_for_xmas=="a_train,a_puppy,a_macbook_pro_the_new_one,a_cat" then puts "success" else raise "EPIC FAIL" end
+begin 
+  a.whoops
+rescue NoMethodError
+  puts "success"
+end
 if a.math_2_plus_3_equals(5) then puts "success" else raise "EPIC FAIL" end
+if WildCardTest.helloDoug=="Doug" then puts "success" else raise "EPIC FAIL" end
+begin
+  WildCardTest.whoops
+rescue NoMethodError
+  puts "success"
+end
